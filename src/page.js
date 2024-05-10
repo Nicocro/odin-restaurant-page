6306d0539e09d5dcfb386ca5226aececd6aa67d3
nicocro <ncroce00@gmail.com>
2024-05-10 13:59:21 +0100

    completed card logic with module

--- src/page.js
+++ src/page.js
@@ -1,9 +1,14 @@
-export function createCrad(title, imgSrc, description) {
-  const card = document.createElement("div").classList("card");
+export function createCard(title, imgSrc, description) {
+  const card = document.createElement("div");
+  card.classList.add("card");
   card.innerHTML = `
     <h3>${title}</h3>
     <img src="${imgSrc}" width="50" height="50" />
     <p>${description}</p>
     `;
   return card;
+}
+
+export function logStuff(string) {
+  console.log(string);
 }
